340ef1db9c8969ecfe82fa81d3d76b8b164e0bb5
Renato <contato.renatofreire@gmail.com>
2018-05-31 18:28:27 -0300

    Creating the Footer component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 import { MuiThemeProvider } from 'material-ui/styles/index';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MuiThemeProvider>
+          
           <Header title='Front-end test'/>
+
+          <Footer />
+
         </MuiThemeProvider>
       </div>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component{
+
+    render(){
+        return(
+            <footer>
+                <p>Desenvolvido por <a href='http://freire.eng.br' target='_new'>Renato Freire</a></p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
