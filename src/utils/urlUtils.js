df13bb9535c82d3b4f07eb65d9f0362fdec71b8a
Renato <contato.renatofreire@gmail.com>
2018-05-31 22:16:56 -0300

    Installing React Router and creating the urlUtils.js which contains the routes

---
+++ src/utils/urlUtils.js
@@ -0,0 +1,5 @@
+export const urls = {
+    home: { name: 'Home', path:'/'},
+    data: { name: 'Transactions', path:'/transactions'},
+    add:  { name: 'Add Transaction',  path:'/add-transaction'},
+};
