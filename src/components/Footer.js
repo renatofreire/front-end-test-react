1949d0bdfb6c2e8ee8bb8bfaf2eb71e855d1d9c0
Renato <contato.renatofreire@gmail.com>
2018-06-01 11:23:17 -0300

    Translating Footer's content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import { Typography } from 'material-ui';
+
 class Footer extends Component{
 
     render(){
         return(
             <footer>
-                <p>Desenvolvido por <a href='http://freire.eng.br' target='_new'>Renato Freire</a></p>
+                
+                <Typography component='p'className='simple-text'>
+                    Developed by <a href='http://freire.eng.br' target='_new'>Renato Freire</a>
+                </Typography>
             </footer>
         );
     }
