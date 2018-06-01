20456e5c1c0f41e28516ac1ace957037ae113157
Renato <contato.renatofreire@gmail.com>
2018-05-31 22:49:34 -0300

    Fixing the header in order to make possible navigation through links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 import { AppBar, IconButton, Toolbar, Typography} from 'material-ui';
-import HomeIcon from 'material-ui-icons/Home';
+import ArrowBack from "material-ui-icons/ArrowBack";
+import HomeIcon from "material-ui-icons/Home";
+
+
+import {Link} from "react-router-dom";
+import {urls} from "../utils/urlUtils";
+
 
 class Header extends Component{
 
@@ -11,9 +17,16 @@ class Header extends Component{
             <React.Fragment>
                 <AppBar position='static'>
                     <Toolbar>
-                        <IconButton color='inherit'>
-                            <HomeIcon />
+                        
+                        <IconButton 
+                            color='inherit'
+                            component={ props => 
+                                <Link to={urls.home.path} {...props} />
+                            }
+                        >
+                            { this.props.currentRoute === urls.home.path? <HomeIcon /> : <ArrowBack /> }
                         </IconButton>
+
                         <Typography type='title' color='inherit'>
                             {this.props.title}
                         </Typography>
